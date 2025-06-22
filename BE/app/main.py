b5d09b34b43aae81fb6ad3df073dfa54c58cb95b
KimYunSeok <kimyunseok@KimYunSeokui-MacBookAir.local>
2025-06-22 21:49:11 +0900

    feature: env 변수 활용

--- BE/app/main.py
+++ BE/app/main.py
@@ -17,3 +17,15 @@ app.add_middleware(
 @app.get("/ping")
 async def pong():
     return {"message": "pong from backend"}
+
+
+
+# 채팅방 생성
+
+# 내 채팅방 목록 조회
+
+# 채팅방 상세 조회(채팅방 띄우기)
+
+# 메시지 목록 조회
+
+# 메시지 전송
