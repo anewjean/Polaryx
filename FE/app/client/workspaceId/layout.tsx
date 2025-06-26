a93c8169b3e3951e8435dbb315c32ef51d1f399f
iamlee103 <iamlee103@gmail.com>
2025-06-26 17:48:21 +0900

    ✨Feat(*): 기본 layout 설정
    @channel/page.tsx: 리다이렉트용 페이지 표시
    @profile/page.tsx: profile 영역 더미 코드 작성
    @sidebar/page.tsx: sidebar 영역 더미 코드 작성
    [channelId]/page.tsx: channel 영역 더미 코드 작성
    workspaceId/layout.tsx: profile 표시 여부에 따라 resizable 동작 구현

--- FE/app/client/workspaceId/layout.tsx
+++ FE/app/client/workspaceId/layout.tsx
@@ -1,27 +1,109 @@
-"use client"
+"use client";
 
-import { ResizablePanelGroup, ResizablePanel, ResizableHandle } from "@/components/ui/resizable"
+import {
+  ResizablePanelGroup,
+  ResizablePanel,
+  ResizableHandle,
+} from "@/components/ui/resizable";
+import { useState } from "react";
 
-export default function workspaceLayout({ children }: { children: React.ReactNode }) {   
-    return (
-        <div className="flex flex-row h-screen">
-            <ResizablePanelGroup direction="horizontal" className="h-full w-full">                
-                <ResizablePanel defaultSize={20} minSize={10} maxSize={40}>
-                    {/* 사이드바 영역*/}
-                    <div className="h-full w-full bg-red-500"></div>                
-                </ResizablePanel>
-                <ResizableHandle />
-                <ResizablePanel defaultSize={80} minSize={20} maxSize={90}> 
-                    {/* 채널 영역*/}
-                    <div className="h-full w-full bg-green-500"></div>               
-                </ResizablePanel>
-                <ResizableHandle />
-                <ResizablePanel defaultSize={20} minSize={10} maxSize={40}> 
-                    {/* 프로필/스레드 영역*/}
-                    <div className="h-full w-full bg-blue-500"></div>                
-                </ResizablePanel>
-                <ResizableHandle />         
-            </ResizablePanelGroup>
-        </div>    
-    )
+// 프로필 표시 테스트 버튼
+export function OpenProfile({
+  isOpen,
+  toggle,
+}: {
+  isOpen: boolean;
+  toggle: () => void;
+}) {
+  return (
+    <button onClick={toggle} className="fixed top right-4 z-50 text-black">
+      {isOpen ? "닫기" : "열기"}
+    </button>
+  );
+}
+
+export default function workspaceLayout({
+  children,
+  channel,
+  sidebar,
+  profile,
+}: {
+  children: React.ReactNode;
+  channel: React.ReactNode;
+  sidebar: React.ReactNode;
+  profile: React.ReactNode;
+}) {
+  const [sidebarWidth, setSidebarWidth] = useState(20);
+  const [profileWidth, setProfileWidth] = useState(20);
+  const [channelWidth, setChannelWidth] = useState(80);
+
+  // 프로필 표시를 위한 state
+  const [isProfileOpen, setIsProfileOpen] = useState(false);
+
+  // 패널 크기 변경 시 상태 업데이트
+  const handleLayout = (sizes: number[]) => {
+    setSidebarWidth(sizes[0]);
+    if (isProfileOpen) {
+      setChannelWidth(sizes[1]);
+      setProfileWidth(sizes[2]);
+    } else {
+      setChannelWidth(sizes[1]);
+    }
+  };
+
+  // 프로필 표시 테스트용 버튼
+  const toggleProfile = () => {
+    if (isProfileOpen) {
+      setChannelWidth(100 - sidebarWidth);
+    } else {
+      setChannelWidth(100 - sidebarWidth - profileWidth);
+    }
+    setIsProfileOpen((prev) => !prev);
+  };
+
+  return (
+    <div className="flex-1 flex flex-row">
+      <OpenProfile isOpen={isProfileOpen} toggle={toggleProfile} />
+      <ResizablePanelGroup
+        direction="horizontal"
+        className="h-full w-full"
+        onLayout={handleLayout}
+      >
+        <ResizablePanel
+          id="sidebar"
+          defaultSize={sidebarWidth}
+          minSize={10}
+          maxSize={30}
+        >
+          {/* 사이드바 영역*/}
+          {sidebar}
+        </ResizablePanel>
+        <ResizableHandle />
+        <ResizablePanel
+          id="channel"
+          defaultSize={channelWidth}
+          minSize={40}
+          maxSize={90}
+        >
+          {/* 채널 영역*/}
+          {channel}
+        </ResizablePanel>
+        {/* 프로필이 열렸을 때만 렌더링 */}
+        {isProfileOpen && (
+          <>
+            <ResizableHandle />
+            <ResizablePanel
+              id="profile"
+              defaultSize={profileWidth}
+              minSize={10}
+              maxSize={30}
+            >
+              {/* 프로필 영역 */}
+              {profile}
+            </ResizablePanel>
+          </>
+        )}
+      </ResizablePanelGroup>
+    </div>
+  );
 }
