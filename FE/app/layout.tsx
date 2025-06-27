63d9c984889b11a615285b6928cb6ae2f7cde086
iamlee103 <iamlee103@gmail.com>
2025-06-27 13:14:57 +0900

    ✨Feat(*): Sidebar, Profile이 Resizable 인터랙션과 Sync되도록 구현

--- FE/app/layout.tsx
+++ FE/app/layout.tsx
@@ -1,17 +1,16 @@
 "use client";
 
-import { notoSansKr, lato } from './ui/fonts';  // 글꼴
+import { notoSansKr, lato } from "./ui/fonts"; // 글꼴
 import "./globals.css";
-export default function ClientLayout({
+
+export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${notoSansKr.variable} ${lato.variable}`}
-      >
+    <html lang="en" className="h-full">
+      <body className={`${notoSansKr.variable} ${lato.variable} min-h-screen`}>
         {children}
       </body>
     </html>
