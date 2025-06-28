00183002dc24eb868ca82a42db820acedfdabe99
SinMoonGh <tity0009@naver.com>
2025-06-28 22:30:10 +0900

    💄Design(workspaceId/layout.tsx, channel/page.tsx, store/channelStore.ts): chatting textarea size 동적으로 변경
    - zustand 설치
    - FE 폴더 하위에 store 폴더 생성
    - zustand를 사용하여 channel size의 state를 받아와서 처리했습니다.

--- FE/app/client/workspaceId/@channel/page.tsx
+++ FE/app/client/workspaceId/@channel/page.tsx
@@ -1,9 +1,19 @@
+"use client";
+
 import TipTap from "@/components/chat-text-area/tiptap";
+import { useChannelStore } from "@/store/channelStore";
 
 export default function ChannelDefault() {
+  const { channelWidth } = useChannelStore();
+
   return (
     <div className="relative text-gray-800">
-      <div className="fixed bottom-0 p-4 w-240">
+      <div
+        className={`fixed bottom-0 p-4`}
+        style={{
+          width: `${channelWidth}%`,
+        }}
+      >
         <TipTap />
       </div>
     </div>
