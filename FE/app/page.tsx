b180586cc5ffa6b8d6695dc4530d7f666a0d9d27
iamlee103 <iamlee103@gmail.com>
2025-06-30 16:01:42 +0900

    👷Chore(*): clerk 관련 코드 추가 삭제

--- FE/app/page.tsx
+++ FE/app/page.tsx
@@ -1,11 +1,7 @@
-import { ClerkProvider } from "@clerk/nextjs";
-
 export default function Page() {
   return (
-    <ClerkProvider>
-      <div>
-        <p>여긴 워크스페이스 선택하는 곳</p>
-      </div>
-    </ClerkProvider>
+    <div>
+      <p>여긴 워크스페이스 선택하는 곳</p>
+    </div>
   );
 }
