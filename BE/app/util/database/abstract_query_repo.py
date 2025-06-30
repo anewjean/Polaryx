090aa6b917b3b9cb111e7794d534f63228b24638
안유진 <yoojin.an0@gmail.com>
2025-07-01 00:52:37 +0900

    ✨Feat(BE/app/util/database/abstract_query_repo.py): db 객체 초기화 및 구현체에서 save 메소드 구현 강제

--- BE/app/util/database/abstract_query_repo.py
+++ BE/app/util/database/abstract_query_repo.py
@@ -1,8 +1,14 @@
 from abc import ABCMeta, abstractmethod
+from BE.app.util.database.db_impl import DBImpl
 
 class AbstractQueryRepo(metaclass=ABCMeta):
-    def __init__(self):
+    def __init__(self, db: DBImpl):
         self.queries = {}
+        self.db = db
     
     def get_sql(self, sql_name):
         return self.queries[sql_name]
+
+    @abstractmethod
+    def save(self, domain_object):
+        pass
