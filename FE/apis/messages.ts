3493db2744a66cf92b0f90a19a9e720bb860a34d
SinMoonGh <tity0009@naver.com>
2025-07-02 11:24:56 +0900

    ✨Feat(messages.ts): 수정, 삭제, 조회 api 생성

---
+++ FE/apis/messages.ts
@@ -0,0 +1,33 @@
+const request = async <T = any>(path: string, options: RequestInit = {}): Promise<T> => {
+  const response = await fetch(path, {
+    headers: { "Content-Type": "application/json" },
+    ...options,
+  });
+
+  if (!response.ok) {
+    const errorText = await response.text();
+    throw new Error(errorText || "서버 에러");
+  }
+
+  return response.json();
+};
+
+export const updateMessage = async (id: number, message: string) => {
+  return request(`/messages/${id}`, {
+    method: "PATCH",
+    body: JSON.stringify({ content: message }),
+  });
+};
+
+export const deleteMessage = async (id: number) => {
+  return request(`/messages/${id}`, {
+    method: "POST",
+    body: JSON.stringify({ id }),
+  });
+};
+
+export const getMessages = async (workspaceId: string, tabId: string) => {
+  return request(`/messages/${workspaceId}/${tabId}`, {
+    method: "GET",
+  });
+};
