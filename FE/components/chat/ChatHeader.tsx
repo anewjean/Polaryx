dfc78b6a40fc8d013b7d1c59b036462823d49573
SinMoonGh <tity0009@naver.com>
2025-07-02 20:41:18 +0900

    💄Design(ChatHeader.tsx): header 부분의 tab button 디자인 수정
    - header에 위치한 tab들의 위치를 수정했습니다.

--- FE/components/chat/ChatHeader.tsx
+++ FE/components/chat/ChatHeader.tsx
@@ -2,23 +2,27 @@ import { MessageCircle, StickyNote } from "lucide-react";
 
 export function ChatHeader() {
   return (
-    <div>
+    <>
       <div className="flex items-center h-[50px] px-[17px]">
         <div className="flex items-center h-[30px] px-[3px] cursor-pointer hover:bg-[#F4F4F4] hover:rounded-md">
           <img src="/jungler.png" className="w-[24px] h-[24px] mr-[8px] rounded-md" />
           <p className="text-l">Dongseok Lee (이동석)</p>
         </div>
       </div>
-      <div className="flex items-center h-[38px] px-[16px] border-b-2">
-        <div className="flex items-center w-[72px] p-[8px] cursor-pointer hover:bg-[#F4F4F4] hover:rounded-t-md">
-          <MessageCircle className="w-[16px] mr-[4px]" />
+      <div className="flex items-center h-[38px] px-[10px] border-b-2">
+        <div className="flex items-center w-[120px] p-[5px] cursor-pointer hover:bg-[#F4F4F4] hover:rounded-t-md">
+          <StickyNote className="w-[16px] mr-[4px]" />
           <p className="text-center text-s-bold">메시지</p>
         </div>
-        <div className="flex items-center w-[72px] p-[8px] cursor-pointer hover:bg-[#F4F4F4] hover:rounded-t-md">
+        <div className="flex items-center w-[120px] p-[5px] cursor-pointer hover:bg-[#F4F4F4] hover:rounded-t-md">
           <StickyNote className="w-[16px] mr-[4px]" />
-          <p className="text-center text-s-bold">캔버스</p>
+          <p className="text-center text-s-bold">학습 자료</p>
+        </div>
+        <div className="flex items-center w-[120px] p-[5px] cursor-pointer hover:bg-[#F4F4F4] hover:rounded-t-md">
+          <StickyNote className="w-[16px] mr-[4px]" />
+          <p className="text-center text-s-bold">링크 추가</p>
         </div>
       </div>
-    </div>
+    </>
   );
 }
