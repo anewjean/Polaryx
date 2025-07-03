34502afaa97d5c2a32374c21560740274eb9a08e
SinMoonGh <tity0009@naver.com>
2025-07-03 22:44:20 +0900

    💄Design(chatEditButton/Button.tsx): chatting 창의 edit button 디자인 수정

--- FE/components/chat/chatEditButton/Button.tsx
+++ FE/components/chat/chatEditButton/Button.tsx
@@ -1,6 +1,6 @@
 export function EditButton(props: { text: string; subtext: string }) {
   return (
-    <button className="flex flex-1 flex-row justify-between hover:bg-blue-500 gap-2 p-4 text-sm overflow-hidden text-gray-900">
+    <button className="flex flex-1 flex-row justify-between hover:bg-blue-500 gap-2 p-4 text-sm text-gray-900">
       <span>{props.text}</span>
       <span>{props.subtext}</span>
     </button>
