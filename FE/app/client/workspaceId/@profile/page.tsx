e7024ccfe8a4a78df859f608331539e384685ae7
iamlee103 <iamlee103@gmail.com>
2025-07-04 19:39:14 +0900

    ⏪Revert(*): 이전 작업 내역 복구

--- FE/app/client/workspaceId/@profile/page.tsx
+++ FE/app/client/workspaceId/@profile/page.tsx
@@ -1,8 +1,94 @@
 "use client";
 
-type ProfileProps = { width: number };
+import { useState, useEffect } from "react";
+import { jwtDecode } from "jwt-decode";
+import { X } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
+import { Mail } from "lucide-react";
+import { SquarePen } from "lucide-react";
+import { CardModal } from "@/components/modal/CardModal";
+import { Input } from "@/components/ui/input";
+import { useProfileStore } from "@/store/profileStore";
+import { getProfile, patchProfile, Profile } from "@/apis/profileApi";
+import { CardFooter } from "@/components/ui/card";
 
-export default function ProfilePage({ width }: ProfileProps) {
+type ProfileProps = {
+  targetId?: string;
+};
+
+export default function ProfilePage({ targetId }: ProfileProps) {
+  const accessToken = localStorage.getItem("accessToken");
+  if (!accessToken) throw new Error("로그인이 필요합니다.");
+
+  // accessToken의 sub 클레임에 있는 userId 추출
+  const { user_id: userId } = jwtDecode<{ user_id: string }>(accessToken);
+
+  // 프로필 닫기 시 실행할 함수형 변수 선언
+  const close = useProfileStore((s) => s.setClose);
+
+  // 프로필 데이터 상태 관리
+  const [profile, setProfile] = useState<Profile | null>(null);
+
+  // 프로필 편집 폼 데이터 상태 관리
+  const [form, setForm] = useState<{
+    nickname: string;
+    phone?: string;
+    github?: string;
+    blog?: string;
+  }>({ nickname: "" });
+
+  // 프로필 조회 중 상태 관리
+  const [loading, setLoading] = useState(true);
+
+  // 프로필 저장 중 상태 관리
+  const [saving, setSaving] = useState(false);
+
+  // 프로필 조회 (페이지 렌더 후 바로 실행 (userId 변경 시 재 실행))
+  useEffect(() => {
+    (async () => {
+      try {
+        setLoading(true);
+        let profileData: Profile;
+        if (targetId) {
+          profileData = await getProfile(targetId);
+
+          // targetId가 없으면 본인 프로필 조회
+        } else {
+          profileData = await getProfile(userId);
+        }
+        setProfile(profileData);
+        setForm({
+          nickname: profileData.nickname,
+          phone: profileData.phone ?? "",
+          github: profileData.github ?? "",
+          blog: profileData.blog ?? "",
+        });
+      } catch (error) {
+        console.error("프로필 조회 실패:", error);
+      } finally {
+        setLoading(false);
+      }
+    })();
+  }, [targetId]);
+
+  // 프로필 수정
+  const saveChange = async () => {
+    if (!profile) return;
+    setSaving(true);
+    try {
+      let updatedProfile: Profile;
+      updatedProfile = await patchProfile(userId, form);
+      setProfile(updatedProfile);
+      close();
+    } catch (error) {
+      console.error(error);
+      alert("프로필 수정에 실패했습니다.");
+    } finally {
+      setSaving(false);
+    }
+  };
+
   
   return (
     <div
