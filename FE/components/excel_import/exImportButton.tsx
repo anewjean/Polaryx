3ad824bddd60d9911821be86880e943b7ea4ce9c
iamlee103 <iamlee103@gmail.com>
2025-07-07 22:49:17 +0900

    🐛Fix(exImprotButton.tsx): exImprotButton.tsx 파일 최신화

--- FE/components/excel_import/exImportButton.tsx
+++ FE/components/excel_import/exImportButton.tsx
@@ -6,53 +6,36 @@ import { usePathname } from "next/navigation";
 import { createUsers } from "@/apis/excelApi";
 import { detectTyposJaccard } from "./detectTyposJaccard";
 import { useMemberStore } from "@/store/memberStore";
-
-export function ExUpload({ buttonName }: { buttonName: string }) {
+export function ExUpload() {
   const workspaceId = usePathname().split("/")[2];
   const inputRef = useRef<HTMLInputElement>(null);
   const { setMemberList } = useMemberStore();
-
   const handleClick = () => {
     inputRef.current?.click();
   };
-
   const handleFile = async (e: React.ChangeEvent<HTMLInputElement>) => {
     const file = e.target.files?.[0];
     if (!file) return;
-
     if (!/\.(xlsx|xls)$/i.test(file.name)) {
       alert("엑셀 파일만 업로드 가능합니다 (.xlsx, .xls)");
       return;
     }
-
-    getWorkspaceColumns();
-
     // excel 파일을 읽어옴
     const data = await file.arrayBuffer();
     const workbook = XLSX.read(data, { type: "array" });
     const sheetName = workbook.SheetNames[0];
     const sheet = workbook.Sheets[sheetName];
     const jsonData = XLSX.utils.sheet_to_json(sheet);
-
     // 엑셀 헤더 file 검사
     const json = XLSX.utils.sheet_to_json(sheet, { header: 1 });
     const uploadedHeaders = json[0];
-
     const typos = detectTyposJaccard(uploadedHeaders as string[]);
     if (typos.length > 0) {
       alert(`오타가 있습니다. ${typos.map((t) => t.wrong).join(", ")}`);
       return;
     }
-
     // 형식에 맞지 않은 user를 제거
-    const { users, errors, total } = filterUsers(jsonData);
-
-    // const memberList = users.map((user) => ({
-    //   name: user.name,
-    //   email: user.email,
-    //   workspace_id: workspaceId,
-    // }));
-
+    const { users, errors } = filterUsers(jsonData);
     const memberList = users.map((user) => ({
       email: user.email,
       name: user.name,
@@ -62,10 +45,8 @@ export function ExUpload({ buttonName }: { buttonName: string }) {
       github: user.github,
       workspace_id: workspaceId,
     }));
-
     // memberList를 store에 저장
     setMemberList(memberList);
-
     try {
       const result = await createUsers(memberList);
       alert(`성공적으로 등록된 유저 수: ${result.success_count}`);
@@ -73,11 +54,10 @@ export function ExUpload({ buttonName }: { buttonName: string }) {
       alert("유저 등록에 실패했습니다.");
     }
   };
-
   return (
     <>
-      <Button onClick={handleClick} variant="link" className="text-white">
-        {buttonName}
+      <Button onClick={handleClick} variant="secondary">
+        Excel에서 가져오기
       </Button>
       <input type="file" accept=".xlsx, .xls" onChange={handleFile} ref={inputRef} className="hidden" />
     </>
