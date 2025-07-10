1d321ed8a961d1909a106380049c49db18c8db40
iamlee103 <iamlee103@gmail.com>
2025-07-10 14:40:19 +0900

    🐛Fix(MiniProfile.tsx): DM 신청 시 전달 payload에 본인 user_id 포함

--- FE/components/chat/MiniProfile.tsx
+++ FE/components/chat/MiniProfile.tsx
@@ -4,6 +4,8 @@ import { useProfileStore } from "@/store/profileStore";
 import { sendDirectMessage } from "@/apis/messageApi";
 import { useParams } from "next/navigation";
 import { useRouter } from "next/navigation";
+import { jwtDecode } from "jwt-decode";
+import { useEffect, useState } from "react";
 
 interface MiniProfileProps {
   senderId: Buffer;
@@ -13,14 +15,26 @@ interface MiniProfileProps {
 
 export function MiniProfile({ senderId, imgSrc, nickname }: MiniProfileProps) {
   // URL에서 workspaceId 추출
+  const router = useRouter();
   const params = useParams();
   const workspaceId = params.workspaceId as string;
 
-  // 라우터
-  const router = useRouter();
-
   // 프로필
   const openProfile = useProfileStore((s) => s.openWithId);
+
+  // 현재 유저 ID 상태 관리
+  const [userId, setUserId] = useState<string | null>(null);
+
+  // 진입 시 유저 ID 획득
+  useEffect(() => {
+    const token = localStorage.getItem("access_token");
+    if (token) {
+      const currentUserId = jwtDecode<{ user_id: string }>(token).user_id;
+      setUserId(currentUserId);
+    } else {
+      router.replace("/");
+    }
+  }, []);
 
   // DM 생성 이벤트 핸들러
   const createDM = async (userIds: string[]) => {
@@ -42,19 +56,20 @@ export function MiniProfile({ senderId, imgSrc, nickname }: MiniProfileProps) {
         <div>
           <div className="ml-0.5 text-m-bold">{nickname}</div>
           <div className="mt-1.5">
-
             {/* DM 버튼 */}
             <Button
               className="cursor-pointer"
               variant="outline"
               size="sm"
               onClick={() => {
-                createDM([senderId.toString()]);
+                if (userId) {
+                  createDM([userId, senderId.toString()]);
+                }
               }}
             >
               <div className="text-s-bold">DM</div>
             </Button>
-            
+
             {/* 프로필 버튼 */}
             <Button onClick={() => openProfile(senderId)} className="ml-1 cursor-pointer" variant="outline" size="sm">
               <div className="text-s-bold">프로필</div>
