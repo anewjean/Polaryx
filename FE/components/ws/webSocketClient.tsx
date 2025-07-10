1b6b06fdb46d666e89540252e94230d5d81d8711
kimyunseok <yunsuk224@gmail.com>
2025-07-11 02:55:28 +0900

    🐛Fix(webSocketClient.tsx): 웹소켓 secure 연결로 변경

--- FE/components/ws/webSocketClient.tsx
+++ FE/components/ws/webSocketClient.tsx
@@ -23,7 +23,7 @@ export const WebSocketClient = ({ workspaceId, tabId }: { workspaceId: string; t
   useEffect(() => {
     console.log("new web sokcet");
     // const socket = new WebSocket(`ws://localhost:8000/api/ws/${workspaceId}/${tabId}`); 개발용
-    const socket = new WebSocket(`ws://jungle-lms.site:8000/api/ws/${workspaceId}/${tabId}`);
+    const socket = new WebSocket(`wss://jungle-lms.site:8000/api/ws/${workspaceId}/${tabId}`);
 
     socketRef.current = socket;
 
