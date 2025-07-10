77add4b86fedacfd86d7f94c4485463e6933a322
kimyunseok <yunsuk224@gmail.com>
2025-07-10 14:50:39 +0900

    🐛Fix(mysql_query_repo.py, auth_service.py): logout 로직 수정.

--- BE/app/service/auth/auth_service.py
+++ BE/app/service/auth/auth_service.py
@@ -101,8 +101,12 @@ class TokenSerive:
     def delete_refresh_token_from_db(data: dict):
         print("in delete_refresh_token_from_db")
         print("data: ", data)
-        sql = query_repo.get_sql("remove_refresh_token_by_user_id_and_token")
+        sql = query_repo.get_sql("find_refresh_token_id_by_user_id_and_token")
         params = {"user_id": data["user_id"], "user_refresh_token": data["user_refresh_token"]}
+        res = db.execute(sql, params)
+        print(res)
+        params = {"refresh_tokens_id": res[0][0]}
+        sql = query_repo.get_sql("remove_refresh_token_by_id")
         db.execute(sql, params)
 
 
