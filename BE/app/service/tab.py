657274d5aa7d9e3607e66f2e86f562fb24becbaa
안유진 <yoojin.an0@gmail.com>
2025-07-10 11:37:42 +0900

    ♻️Refactor(*): sections.sub_id, tabs.sub_section_id 삭제 반영

--- BE/app/service/tab.py
+++ BE/app/service/tab.py
@@ -10,12 +10,12 @@ class TabService:
     def is_tab_name_duplicate(self, workspace_id: int, section_id:int, name: str):
         return self.repo.is_duplicate(workspace_id, section_id, name)
 
-    def create_tab(self, user_ids: List, workspace_id: int, tab_name: str, section_id: int, subsection_id: int):
+    def create_tab(self, user_ids: List, workspace_id: int, tab_name: str, section_id: int):
         is_valid = self.repo.validate_section_in_workspace(section_id, workspace_id)
         if not is_valid:
             raise HTTPException(status_code=400, detail="섹션이 이 워크스페이스에 속하지 않습니다.")
-        self.repo.insert(workspace_id, tab_name, section_id, subsection_id)
-        rows = self.repo.find_by_uq(workspace_id, tab_name, section_id, subsection_id)
+        self.repo.insert(workspace_id, tab_name, section_id)
+        rows = self.repo.find_by_uq(workspace_id, tab_name, section_id)
         tab_id = rows[0][0]
         self.invite_members(workspace_id, tab_id, user_ids)
         return rows
