0c3c9f7d42a2267008c4bd4e94ce38e362e779bc
안유진 <yoojin.an0@gmail.com>
2025-07-10 18:26:29 +0900

    ✨Feat(*): DM 요청 시 이미 존재하는 탭이면 기존 탭 아이디 반환

--- BE/app/router/direct_message.py
+++ BE/app/router/direct_message.py
@@ -7,14 +7,13 @@ from app.service.direct_message import DMService
 
 
 router = APIRouter(prefix="/workspaces")
-tab_service = TabService()
 dm_service = DMService()
 
 # 탭 생성
 @router.post("/{workspace_id}/dms", response_model=CreateTabResponse)
-def create_tab(workspace_id: int, tab_data: CreateTabRequest):
+def get_tab(workspace_id: int, tab_data: CreateTabRequest):
     member_names = dm_service.find_member_names(tab_data.user_ids)
     tab_name = ", ".join(member_names)
     section_id = 4
-    rows = tab_service.create_tab(tab_data.user_ids, workspace_id, tab_name, section_id)
+    rows = dm_service.get_tab(tab_data.user_ids, workspace_id, tab_name, section_id)
     return CreateTabResponse.from_row(rows[0])
--- BE/app/service/direct_message.py
+++ BE/app/service/direct_message.py
@@ -1,9 +1,17 @@
 from typing import List
 from app.repository.direct_message import DMRepository
+from app.service.tab import TabService
 
 class DMService:
     def __init__(self):
         self.repo = DMRepository()
+        self.tab_service = TabService()
+
+    def get_tab(self, user_ids: List, workspace_id: int, tab_name: str, section_id: int):
+        tab = self.tab_service.find_by_unique_key(workspace_id, tab_name, section_id)
+        if tab:
+            return tab
+        return self.tab_service.create_tab(user_ids, workspace_id, tab_name, section_id)
 
     def find_member_names(self, user_ids: List) -> List:
         rows = self.repo.find_member_names(user_ids)
