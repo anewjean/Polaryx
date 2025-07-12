15d0fc84cd786a30f83eaeaf0b1f5f40c27edbc4
SinMoonGh <tity0009@naver.com>
2025-07-12 20:42:37 +0900

    🐛Fix(UserMenuItem, webSocketClient): ci-cd bug 해결
    - UserMenuItem의 img tag -> Image사용
    - useEffect에 fileUrl이 빠져있어서 추가

--- FE/components/tab/UserMenuItem.tsx
+++ FE/components/tab/UserMenuItem.tsx
@@ -5,6 +5,7 @@ import { Plus, CircleCheck, Mail, SquareUserRound, AtSign } from "lucide-react";
 import clsx from "clsx";
 import { Member } from "@/apis/tabApi";
 import { Button } from "@/components/ui/button";
+import Image from "next/image";
 
 export interface UserMenuItemProps {
   user: Member;
@@ -13,25 +14,39 @@ export interface UserMenuItemProps {
   isSelected?: boolean;
 }
 
-export function UserMenuItem({ user, mode = "default", onClick, isSelected }: UserMenuItemProps) {
+export function UserMenuItem({
+  user,
+  mode = "default",
+  onClick,
+  isSelected,
+}: UserMenuItemProps) {
   return (
     <SidebarMenuItem key={user.user_id}>
       <SidebarMenuButton
-        className={clsx("py-8 px-4 hover:bg-gray-200 flex justify-between items-center rounded-none", {
-          "cursor-pointer": !!onClick,
-        })}
+        className={clsx(
+          "py-8 px-4 hover:bg-gray-200 flex justify-between items-center rounded-none",
+          {
+            "cursor-pointer": !!onClick,
+          },
+        )}
         onClick={onClick}
       >
         {/* 공통: 프로필 + 닉네임 + 역할 */}
         <div className="flex flex-row w-full justify-between items-center">
           <div className="flex items-end gap-2">
-            <img
+            <Image
               src={user.image || "/user_default.png"}
               alt={user.nickname}
+              width={28}
+              height={28}
               className="w-[28px] aspect-square bg-gray-400 rounded-md"
             />
-            <span className="text-lg font-bold text-gray-800 truncate">{user.nickname}</span>
-            <span className="text-sm font-bold text-gray-400 truncate">{user.role}</span>
+            <span className="text-lg font-bold text-gray-800 truncate">
+              {user.nickname}
+            </span>
+            <span className="text-sm font-bold text-gray-400 truncate">
+              {user.role}
+            </span>
           </div>
 
           {/* default: DM 버튼 + Profile 버튼 */}
