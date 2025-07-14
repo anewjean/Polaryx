5d318d7a7cbdbd74ef57991e18f9cd8cdd29fa38
SinMoonGh <tity0009@naver.com>
2025-07-14 20:13:53 +0900

    Merge branch 'main' of origin/main

--- BE/app/repository/direct_message.py
+++ BE/app/repository/direct_message.py
@@ -8,7 +8,7 @@ class DMRepository(AbstractQueryRepo):
         db = DBFactory.get_db("MySQL")
         super().__init__(db)
 
-    def find_member_names(self, user_ids: List[str]):     
+    def find_member_names(self, user_ids: List[str]):   
         user_id_bytes = [UUID(user_id).bytes for user_id in user_ids]
         format_strings = ','.join(['%s'] * len(user_id_bytes))               
         query = f"SELECT user_id, nickname FROM workspace_members WHERE user_id IN ({format_strings}) AND deleted_at IS NULL"        
