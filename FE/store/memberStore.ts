0995b981ccb131b43f47645d43f57ad46d9f03d7
iamlee103 <iamlee103@gmail.com>
2025-07-14 16:13:19 +0900

    ⏪Revert(memberStore.ts): 기존 인터페이스 복구 (Member)

--- FE/store/memberStore.ts
+++ FE/store/memberStore.ts
@@ -4,12 +4,10 @@ import { create } from "zustand";
 interface Member {
   email: string;
   name: string;
-  role_id: number;
-  role_name: string;
-  group_name?: string[];
-  group_id?: number[];
-  github?: string;
-  blog?: string;
+  role: string;
+  group: string;
+  github: string;
+  blog: string;
   workspace_id: string;
 }
 
