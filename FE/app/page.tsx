64de21b1dc1e72212fcb881077dbef80df6eb601
echpark <ghkqh09@gmail.com>
2025-07-16 06:00:48 +0900

    🐛Fix(FE/app/page.tsx): 로그인 화면 CSS 수정
    화면 크기 줄이면 생기는 오류 해결

--- FE/app/page.tsx
+++ FE/app/page.tsx
@@ -88,12 +88,12 @@ export default function Page() {
       <Stars count={150} />
 
       {/* 로고 */}
-      <div className="justify-center gap-10 relative z-10 p-8 bg-black backdrop-blur-md rounded-2xl flex flex-col items-center w-[30%] mt-15">
+      <div className="justify-center gap-10 z-10 p-8 bg-black backdrop-blur-md rounded-2xl flex flex-col items-center w-[30%] mt-15">
         <div className="flex flex-row items-center mb-15">
           <img src="./logo.png" alt="SLAM Logo" className="w-15 h-15" />
-          <h1 className="ml-3 zen-antique-soft-regular text-5xl font-extrabold text-white text-center tracking-[0.4rem]">
+          <h1 className="ml-3 zen-antique-soft-regular text-5xl font-extrabold text-white text-center tracking-[0.4rem] whitespace-nowrap">
             Polaris
-            <span className="relative ml-1 -translate-y-7 inline-block text-blue-200">
+            <span className="ml-1 -translate-y-7 inline-block text-blue-200">
               .
             </span>
           </h1>
