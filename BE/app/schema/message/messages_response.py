b927a3369a167c7c254c0b41874b890d4a774b30
kimyunseok <yunsuk224@gmail.com>
2025-07-18 23:00:03 +0900

    ✨Feat(*): 이모지 백 로직 구현

--- BE/app/schema/message/messages_response.py
+++ BE/app/schema/message/messages_response.py
@@ -19,9 +19,17 @@ class MessageSchema(BaseModel):
     updated_at: datetime | None
     deleted_at: datetime | None
     file_url: Optional[str] = None
+    e_check_cnt: int
+    e_clap_cnt: int
+    e_like_cnt: int
+    my_toggle: Optional[str] = None
 
     @classmethod
     def from_row(cls, row: tuple) -> MessageSchema:
+        res = []
+        if row[14] == 1: res.append("check")
+        if row[15] == 1: res.append("clap")
+        if row[16] == 1: res.append("like")
         return cls(
             msg_id=row[0],
             tab_id=row[1],
@@ -34,6 +42,10 @@ class MessageSchema(BaseModel):
             updated_at=row[8],
             deleted_at=row[9],
             file_url=row[10] if len(row) > 10 else None,
+            e_check_cnt=row[11],
+            e_clap_cnt=row[12],
+            e_like_cnt=row[13],
+            my_toggle=res
         )
 
 
