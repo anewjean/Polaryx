1ee3e3fe5ada4f820f49b19952897082455b32f2
SinMoonGh <tity0009@naver.com>
2025-07-19 00:56:27 +0900

    💄Design(fileUpload/FileUpload.tsx, utils/fileUtils.ts): file upload button 디자인 변경

--- FE/utils/fileUtils.ts
+++ FE/utils/fileUtils.ts
@@ -6,3 +6,28 @@ export const convertFileToBase64 = (file: File): Promise<string> => {
     reader.readAsDataURL(file);
   });
 };
+
+export const getFileExtension = (fileName: string): string => {
+  return fileName.split(".").pop()?.toUpperCase() || "";
+};
+
+export const getFileIcon = (fileName: string): string => {
+  const ext = fileName.split(".").pop()?.toLowerCase() || "";
+  console.log(ext);
+
+  switch (ext) {
+    case "pdf":
+      return "/file-pdf.png"; // PDF 전용 아이콘
+    case "doc":
+    case "docx":
+      return "/file-word.png"; // Word 전용 아이콘
+    case "xls":
+    case "xlsx":
+      return "/file-excel.png"; // Excel 전용 아이콘
+    case "ppt":
+    case "pptx":
+      return "/file-powerpoint.png"; // PowerPoint 전용 아이콘
+    default:
+      return "/upload_default.png"; // 기본 아이콘
+  }
+};
