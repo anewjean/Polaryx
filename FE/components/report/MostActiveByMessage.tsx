d38b04ae50a29b0e54865781dfc29e026d319986
iamlee103 <iamlee103@gmail.com>
2025-07-20 03:11:08 +0900

    💄Design(*): 리포트 페이지 디자인 개선

--- FE/components/report/MostActiveByMessage.tsx
+++ FE/components/report/MostActiveByMessage.tsx
@@ -79,7 +79,7 @@ export function MostActiveByMessage() {
         </div>
         {/* 기간 필터 */}
         <Select value={timeRange} onValueChange={setTimeRange}>
-          <SelectTrigger className="w-[150px] rounded-lg ml-auto" aria-label="Select a value">
+          <SelectTrigger className="w-[145px] rounded-lg ml-auto" aria-label="Select a value">
             <SelectValue placeholder="Select time range" />
           </SelectTrigger>
           <SelectContent className="rounded-xl">
